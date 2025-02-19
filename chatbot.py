dfc2097b82035700aa02be48d14150b80ef55832
Pedro <pedrolodonio20@gmail.com>
2025-02-19 17:15:17 -0300

    adicionando a classe chatbot e a função construtora

--- chatbot.py
+++ chatbot.py
@@ -3,3 +3,15 @@ import gradio as gr
 import requests
 import json
 
+#Define classe chatbot
+class chatbot:
+    #Função construtora
+    def __init__(self,api_key,model,provider = "openai"):
+        self.api_key = api_key
+        self.model = model
+        self.provider = provider
+        self.url = "http//:api.unify.ai/vO/chat/completions"
+        self.header = {"Authorization": f"Bearer{self.api_key}"}
+        self.headers = {"Content-Type": "application/json"}
+
+    
