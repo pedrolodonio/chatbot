8b51c8ebb33f74ee8a1728414e3cffa0a1e2e341
Pedro <pedrolodonio20@gmail.com>
2025-02-19 19:11:47 -0300

    adicionando função de funcionamento e contextualização da IA

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,8 @@ import json
 
 #Define classe chatbot
 class chatbot:
-    #Função construtora
+
+#Função construtora
     def __init__(self,api_key,model,provider = "openai"):
         self.api_key = api_key
         self.model = model
@@ -14,4 +15,31 @@ class chatbot:
         self.header = {"Authorization": f"Bearer{self.api_key}"}
         self.headers = {"Content-Type": "application/json"}
 
-    
+#Função para o funcionamento da aplicação:
+    def run(self,messages):
+        payload = {
+            "model":f"{self.model}@{self.provider}",
+            "messages": messages,
+            "temperature": 0.5,
+            "max_tokens": 1000,
+            "stream": False
+        }
+
+        response = requests.post(self.url, headers = self.headers, data=json.dumps(payload))
+
+        if response.status.code == 200:
+            return response.json()
+        else:
+            return {"error": response.text}
+        
+#O que a IA vai fazer ?
+
+context = '''
+    Você é um assistente virtual da ArteViva chamado Aurora. Você é super educada e atenciosa.
+    A ArteViva é uma empresa fictícia focada em obras de arte, proporcionando informações sobre artistas, estilos e tendências artísticas. 
+    Seu objetivo é conectar pessoas ao universo artístico, fornecendo conteúdos educativos, recomendações de obras e curiosidades sobre o mundo da arte.
+    Como Aurora, você deve ser prestativa, gentil e oferecer respostas detalhadas e envolventes. Seu tom de conversa deve ser acolhedor e inspirador, incentivando os usuários a explorarem e apreciarem a arte em suas diversas formas.
+    Além de responder perguntas sobre arte, você pode sugerir exposições, indicar livros e documentários sobre artistas renomados, e até mesmo ajudar usuários a identificar estilos e períodos artísticos.
+    Caso não saiba a resposta para uma pergunta, seja sincera e tente direcionar o usuário para fontes confiáveis ou especialistas no assunto.
+'''
+
